98e2e4ac085dd34fab24e6913e4dd7c68b1c8710
archae0pteryx <github@pocketcereal.com>
2023-09-05 12:58:47 -0700

    read current and skip if have

--- src/file_io.ts
+++ src/file_io.ts
@@ -1,13 +1,18 @@
 import fs from 'fs'
 import path from 'path'
+import { execSync } from 'child_process'
+
+export const absDataPath = () => {
+  const targetRootDir = path.join(__dirname, process.env.DATA_DIR || 'data')
+  return targetRootDir
+}
 
 export const pathFromUrl = (url: string) => {
-  const targetRootDir = path.join(__dirname, process.env.DATA_DIR || 'data')
+  const targetRootDir = absDataPath()
   const urlObj = new URL(url)
   const hostname = urlObj.hostname
   const pathname = urlObj.pathname
   const actualPath = `${targetRootDir}/${hostname}${pathname}`
-
   if (actualPath.endsWith('/')) {
     return actualPath + 'index.html'
   }
@@ -19,7 +24,47 @@ export const pathFromUrl = (url: string) => {
   return actualPath
 }
 
+export const listAllFilesInDir = (dir: string): string[] => {
+  try {
+      const command = `find ${dir} -type f`
+      const currentFiles = execSync(command).toString().trim().split('\n')
+      return currentFiles
+  } catch (err: any) {
+    console.log('Directory not found or empty. continuing...')
+    return []
+  }
+}
+
+export const allFilesFromUrl = (url: string): string[] => {
+  const localDir = absDataPath()
+  const hostname = new URL(url).hostname
+  const allFilesArr = listAllFilesInDir(`${localDir}/${hostname}`)
+  return allFilesArr
+}
+
+// export const filenameFromUrl = (url: string): string => {
+//   const urlObj = new URL(url)
+//   const segments = urlObj.pathname.split('/')
+//   const filename = segments.pop() || 'index.html'
+//   return filename
+// }
+
+export const dirFromUrl = (url: string): string => {
+  const filename = pathFromUrl(url)
+  const split = filename.split('/')
+  split.pop()
+  return split.join('/')
+}
+
 export const writeLocalHtml = (url: string, content: string) => {
+  const hostname = new URL(url).hostname
+  const targetRootDir = absDataPath()
+  const rootDomainDir = `${targetRootDir}/${hostname}`
+  if (!fs.existsSync(rootDomainDir)) {
+    fs.mkdirSync(rootDomainDir, {
+      recursive: true,
+    })
+  }
   const path = pathFromUrl(url)
   const dir = path.split('/').slice(0, -1).join('/')
   if (!fs.existsSync(dir)) {
@@ -29,3 +74,9 @@ export const writeLocalHtml = (url: string, content: string) => {
   }
   fs.writeFileSync(path, content)
 }
+
+export const loadFromLocal = (url: string) => {
+  const path = pathFromUrl(url)
+  return fs.readFileSync(path, 'utf8')
+}
+
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,16 +1,20 @@
 import puppeteer from 'puppeteer'
 import { DESKTOP_USER_AGENTS } from './uas'
-import { alreadyHaveHtml } from './check'
-import { writeLocalHtml } from './file_io'
+import { loadFromLocal, pathFromUrl, writeLocalHtml, allFilesFromUrl } from './file_io';
 import { extractLinksFromHtml } from './urls'
-
-const MAX_EXTRACT_COUNT = 100
+import colors from 'colors'
 
 const SEEN = new Set()
 const QUEUE = new Set()
 
-export const scrape = async (url: string) => {
+export const scrape = async (initialUrl: string) => {
+  let count = 0
   const now = new Date()
+
+  const seenFiles = allFilesFromUrl(initialUrl)
+  seenFiles.forEach((file) => SEEN.add(file))
+
+  QUEUE.add(initialUrl)
 
   const browser = await puppeteer.launch({
     headless: 'new',
@@ -20,38 +24,30 @@ export const scrape = async (url: string) => {
 
   await page.setUserAgent(DESKTOP_USER_AGENTS[0])
   await page.setViewport({ width: 1920, height: 1080 })
-  await page.goto(url, { waitUntil: 'networkidle0' })
 
-  const actualUrl = page.url()
-  const content = await page.content()
-
-  writeLocalHtml(actualUrl, content)
-
-  const hostname = new URL(actualUrl).hostname
-
-  const initialList = extractLinksFromHtml(hostname, content)
-  initialList.forEach((href) => QUEUE.add(href))
-
-  while (QUEUE.size > 0 && SEEN.size < MAX_EXTRACT_COUNT) {
-    console.log(`current url: ${url}`)
-    console.log(`Queue size: ${QUEUE.size}`)
-    console.log(`seen size: ${SEEN.size}`)
-
-    const alreadyHave = alreadyHaveHtml(url)
-    if (alreadyHave) {
-      console.log(`We already have this file: ${url}`)
-      continue
-    }
-
-    if (SEEN.has(url)) {
-      console.log(`Already seen: ${url}`)
-      continue
-    }
-
+  while (QUEUE.size > 0 || count === 0) {
     const href = QUEUE.values().next().value
     QUEUE.delete(href)
+
+
+    const target = colors.blue(`[target]: ...${href.slice(-20)}`)
+    const queue = colors.grey(`[queue]: ${QUEUE.size}`)
+    const seen = colors.grey(`[seen]: ${SEEN.size}`)
+
+    console.log(`${target} ${queue} ${seen}`)
+
+    const localFilePath = pathFromUrl(href)
+    const alreadyHave = SEEN.has(localFilePath)
+    if (alreadyHave) {
+      const html = loadFromLocal(href)
+      const hrefList = extractLinksFromHtml(href, html)
+      hrefList.forEach((h) => QUEUE.add(h))
+      console.log(colors.yellow(`[skipped]: ${href}`))
+      count++
+      continue
+    }
+
     try {
-      console.log(`visiting: ${href}`)
       await page.goto(href, { waitUntil: 'networkidle0' })
 
       const html = await page.content()
@@ -59,20 +55,20 @@ export const scrape = async (url: string) => {
 
       writeLocalHtml(actualUrl, html)
 
-      SEEN.add(href)
+      SEEN.add(localFilePath)
 
       const hrefList = extractLinksFromHtml(actualUrl, html)
-      console.log(`adding ${hrefList.length} links to queue`)
       hrefList.forEach((href) => QUEUE.add(href))
     } catch (err: any) {
-      console.error(`Failed: ${url}`, err.message)
-      SEEN.add(href)
-      continue
+      console.error(colors.red(`Failed: ${initialUrl}`), err.message)
+      SEEN.add(localFilePath)
+    } finally {
+      count++
     }
   }
   const endTime = new Date()
   const timeDiff = endTime.getTime() - now.getTime()
   const seconds = timeDiff / 1000
-  console.log(`Took ${seconds} seconds`)
+  console.log(colors.green(`Took ${seconds} seconds`))
   await browser.close()
 }
