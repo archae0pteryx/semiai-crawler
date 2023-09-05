5500ac9d3bd09aad5aa03ae26eec11ccedbe15a7
archae0pteryx <github@pocketcereal.com>
2023-09-05 10:09:12 -0700

    update tests

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,7 @@
 import { scrape } from './scraper'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 ;(async () => {
   await scrape('https://python.langchain.com/docs/get_started/introduction.html')
